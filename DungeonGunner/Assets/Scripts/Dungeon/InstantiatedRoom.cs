69fc6fe0785321ffa7e16c0ac989530a454e4945
OlegVishnivetsky <vorboleg@gmail.com>
2023-03-01 20:58:06 +0200

    Update InstantiatedRoom.cs
    - Added Block of unused doorways mechanic

--- DungeonGunner/Assets/Scripts/Dungeon/InstantiatedRoom.cs
+++ DungeonGunner/Assets/Scripts/Dungeon/InstantiatedRoom.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -27,6 +28,8 @@ public class InstantiatedRoom : MonoBehaviour
     public void Initialise(GameObject roomGameobject)
     {
         PopulateTilemapMemberVariables(roomGameobject);
+
+        BlockOfUnusedDoorways();
 
         DisableCollisionTilemapRenderer();
     }
@@ -62,6 +65,100 @@ public class InstantiatedRoom : MonoBehaviour
             else if (tilemap.gameObject.tag == StringConstants.minimapTilemapTag)
             {
                 minimapTilemap = tilemap;
+            }
+        }
+    }
+
+    private void BlockOfUnusedDoorways()
+    {
+        foreach (Doorway doorway in room.doorWayList)
+        {
+            if (doorway.isConnected)
+                continue;
+
+            if (collisionTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(collisionTilemap, doorway);
+            }
+
+            if (minimapTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(minimapTilemap, doorway);
+            }
+
+            if (groundTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(groundTilemap, doorway);
+            }
+
+            if (decoration1Tilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(decoration1Tilemap, doorway);
+            }
+
+            if (decoration2Tilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(decoration2Tilemap, doorway);
+            }
+
+            if (frontTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(frontTilemap, doorway);
+            }
+        }
+    }
+
+    private void BlockDoorwayOnTilemapLayer(Tilemap tilemap, Doorway doorway)
+    {
+        switch (doorway.orientation)
+        {
+            case Orientation.North:
+            case Orientation.South:
+                BlockDoorwayHorizontally(tilemap, doorway);
+                break;
+
+            case Orientation.East:
+            case Orientation.West:
+                BlockDoorwayVertically(tilemap, doorway);
+                break;
+
+            case Orientation.None:
+                break;
+        }
+    }
+
+    private void BlockDoorwayHorizontally(Tilemap tilemap, Doorway doorway)
+    {
+        Vector2Int startPosition = doorway.doorwayStartCopyPosition;
+        
+        for (int xPos = 0; xPos < doorway.doorwayCopyTileWidth; xPos++)
+        {
+            for (int yPos = 0; yPos < doorway.doorwayCopyTileHeight; yPos++)
+            {
+                Matrix4x4 transformMatrix = tilemap.GetTransformMatrix(new Vector3Int(startPosition.x + xPos, startPosition.y - yPos, 0));
+
+                tilemap.SetTile(new Vector3Int(startPosition.x + 1 + xPos, startPosition.y - yPos, 0),
+                    tilemap.GetTile(new Vector3Int(startPosition.x + xPos, startPosition.y - yPos, 0)));
+
+                tilemap.SetTransformMatrix(new Vector3Int(startPosition.x + 1 + xPos, startPosition.y - yPos, 0), transformMatrix);
+            }
+        }
+    }
+
+    private void BlockDoorwayVertically(Tilemap tilemap, Doorway doorway)
+    {
+        Vector2Int startPosition = doorway.doorwayStartCopyPosition;
+
+        for (int yPos = 0; yPos < doorway.doorwayCopyTileHeight; yPos++)
+        {
+            for (int xPos = 0; xPos < doorway.doorwayCopyTileWidth; xPos++)
+            {
+                Matrix4x4 transformMatrix = tilemap.GetTransformMatrix(new Vector3Int(startPosition.x + xPos, startPosition.y - yPos, 0));
+
+                tilemap.SetTile(new Vector3Int(startPosition.x + xPos, startPosition.y - 1 - yPos, 0),
+                    tilemap.GetTile(new Vector3Int(startPosition.x + xPos, startPosition.y - yPos, 0)));
+
+                tilemap.SetTransformMatrix(new Vector3Int(startPosition.x + xPos, startPosition.y - 1 - yPos, 0), transformMatrix);
             }
         }
     }
